55c61c3259146366624fc6f29a1874e9174db560
Trần Tiến Đạt <153429234+TranTienDat1@users.noreply.github.com>
2025-03-05 09:03:03 +0700

    Update Kiemtraloi.java
    bị thừa imporrt

--- Kiemtraloi.java
+++ Kiemtraloi.java
@@ -3,7 +3,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.EventQueue; 
+import java.awt.EventQueue; // thừa
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
